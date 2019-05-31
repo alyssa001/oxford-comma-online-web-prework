45089718bf54de1f8dc1da1b1e61b3fa77a3b3f7
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 02:02:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.count == 2 
     return array.join(" and ")
-  end 
+  else
+    lastStr = array.pop
+    array.join(", ") << ", and #{lastStr}"
 end
